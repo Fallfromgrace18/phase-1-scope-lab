277fbd65e1054c55f47f858b04355cfa4ebbab35
Fallfromgrace18 <149462981+Fallfromgrace18@users.noreply.github.com>
2024-04-14 16:14:05 -0400

    completed

--- index.js
+++ index.js
@@ -1,1 +1,31 @@
-// Write your solution in this file!
+var customerName='bob';
+
+
+function upperCaseCustomerName(){
+ customerName = customerName.toUpperCase();
+}
+console.log(customerName)
+
+function setBestCustomer() {
+  bestCustomer = 'not bob';
+
+ console.log('not bob')
+}
+
+setBestCustomer(bestCustomer)
+
+function overwriteBestCustomer(){
+    bestCustomer= 'maybe bob'
+    console.log (bestCustomer)
+}
+  function changeLeastFavoriteCustomer(){
+    const leastFavoriteCustomer='mom'
+    leastFavoriteCustomer.toUpperCase()
+    leastFavoriteCustomer='MOM'
+  console.log(leastFavoriteCustomer)
+  }
+
+
+
+
+   
